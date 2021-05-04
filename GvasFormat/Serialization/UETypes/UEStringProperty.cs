18ef40cb62ea5749ac0c05bc6418907df881bcff
RagingLightning <RagingLightningCode@gmail.com>
2021-05-04 14:18:37 +0200

    fix #3

--- GvasFormat/Serialization/UETypes/UEStringProperty.cs
+++ GvasFormat/Serialization/UETypes/UEStringProperty.cs
@@ -18,9 +18,12 @@ namespace GvasFormat.Serialization.UETypes
                 var terminator = reader.ReadByte();
                 if (terminator != 0)
                     throw new FormatException($"Offset: 0x{reader.BaseStream.Position - 1:x8}. Expected terminator (0x00), but was (0x{terminator:x2})");
+                Value = reader.ReadUEString(valueLength);
+            } else
+            {
+                Value = reader.ReadUEString();
             }
 
-            Value = reader.ReadUEString(valueLength);
         }
 
         public override void SerializeProp(BinaryWriter writer)
